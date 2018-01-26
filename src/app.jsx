9f41e715cf34391713dea605c162ead28757a6be
aploftus <aploftus@users.noreply.github.com>
2018-01-26 11:07:49 -0800

    (fix) Update path to main.css

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles/main.css';
+import './main.css';
 
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
